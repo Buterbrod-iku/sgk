e027d7b06e53053d42605ee8db1cd83dfa91caa4
Buterbrod-iku <melkov750@gmail.com>
2023-07-11 19:21:54 +0700

    routing

--- src/components/routing/form/buttonMenu/buttonMenu.tsx
+++
@@ -1,9 +0,0 @@
-import style from './buttonMenu.module.scss'
-
-export default function ButtonMenu(props) {
-    return (
-        <>
-            <button className={style.button} onClick={props.onClick} style={props.color}>{props.text}</button>
-        </>
-    )
-}
--- src/components/routing/form/inputForm/inputForm.tsx
+++
@@ -1,9 +0,0 @@
-import style from './inputForm.module.scss'
-
-export default function InputForm(props) {
-    return (
-        <>
-            <input type="text" className={style.input} placeholder={props.placeholder} style={props.styles}/>
-        </>
-    )
-}
