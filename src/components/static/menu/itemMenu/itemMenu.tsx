f4bc176b45e86757cd665e9ad8c0e4e3809b3f93
Buterbrod-iku <melkov750@gmail.com>
2023-07-19 02:04:10 +0700

    fix open request

--- src/components/static/menu/itemMenu/itemMenu.tsx
+++ src/components/static/menu/itemMenu/itemMenu.tsx
@@ -1,13 +1,15 @@
 import style from './itemMenu.module.scss'
 
+
 type Item = {
-    item: string;
+    item: string,
+    iconURL: string
 }
 
 export default function ItemMenu(props : Item) {
     return (
         <div className={style.main}>
-            <div className={style.ico}></div>
+            <div className={style.ico} style={{background: `url(${props.iconURL})`}}></div>
             <p className={style.text}>{props.item}</p>
         </div>
     )
