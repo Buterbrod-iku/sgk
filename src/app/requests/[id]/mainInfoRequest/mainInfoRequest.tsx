e534855ff8361d41c4fb12f75e7f4d0452d9187a
Buterbrod-iku <melkov750@gmail.com>
2023-07-20 19:39:56 +0700

    test

--- src/app/requests/[id]/mainInfoRequest/mainInfoRequest.tsx
+++ src/app/requests/[id]/mainInfoRequest/mainInfoRequest.tsx
@@ -14,6 +14,7 @@ const formatTime = (time) => {
 }
 
 export default function MainInfoRequest(props) {
+    // поправить
     const [fullPassenger, setFullPassenger] = useState(false);
     const [fullPoint, setFullPoint] = useState(false);
 
