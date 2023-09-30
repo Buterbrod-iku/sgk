a4311b84706e8768b787372e3873cca675b21ca7
Buterbrod-iku <melkov750@gmail.com>
2023-09-30 17:10:10 +0700

    final pagination

--- src/app/requests/pagination/pagination.tsx
+++ src/app/requests/pagination/pagination.tsx
@@ -5,7 +5,7 @@ const PointPagination = ({number, paginate, focus}) => {
     return(
         <>
             <a href="!#" onClick={(e) => {e.preventDefault(); paginate(number)}} className={style.a}>
-                <li key={number} className={style.li} style={focus ? {background: "red"} : {}}>
+                <li key={number} className={style.li} style={focus ? {background: "rgb(0, 66, 92)", color: "white"} : {}}>
                     {number}
                 </li>
             </a>
@@ -15,7 +15,7 @@ const PointPagination = ({number, paginate, focus}) => {
 
 const CenterPagination = () => {
     return(
-        <li className={style.li}>
+        <li className={style.liCenter}>
             ...
         </li>
     );
@@ -29,31 +29,68 @@ const Pagination = ({currentPage, totalCount, perPage, paginate, nextPage, prevP
     }
 
     let dynamicPagination
+    let typeCountPageNumbers = true
 
-    if(currentPage <= 3){
-        dynamicPagination = pageNumbers.slice(1, 5)
-    } else if(currentPage >= pageNumbers.length - 1){
-        dynamicPagination = pageNumbers.slice(pageNumbers.length - 5, pageNumbers.length + 1)
+    //провека на то какую пагинацию рендерим
+    if(pageNumbers.length > 6){
+        // выделяем часть всех страниц и выводим
+        typeCountPageNumbers = true
+        if(currentPage <= 3){
+            dynamicPagination = pageNumbers.slice(1, 5)
+        } else if(currentPage >= pageNumbers.length - 2){
+            dynamicPagination = pageNumbers.slice(pageNumbers.length - 5, pageNumbers.length - 1)
+        } else {
+            dynamicPagination = pageNumbers.slice(currentPage - 2, currentPage + 1)
+        }
     } else {
-        dynamicPagination = pageNumbers.slice(currentPage - 3, currentPage + 3)
+        typeCountPageNumbers = false
     }
+
 
     return (
         <div className={style.main}>
-            <li className={style.li} onClick={prevPage}>
+            <li className={style.liPrev} onClick={prevPage}>
                 Предыдущая
             </li>
-            <ul className={style.ul}>
-                <PointPagination focus={currentPage === 1} number={1} paginate={startPage}/>
-                {
-                    dynamicPagination.map(number =>  (
-                        <PointPagination focus={currentPage === number} number={number} paginate={paginate}/>
-                    ))
-                }
-                <PointPagination focus={currentPage === pageNumbers[pageNumbers.length-1]} number={pageNumbers[pageNumbers.length-1]} paginate={paginate}/>
 
+            <ul className={style.ul}>
+                {/*если элементов больше 6, то выводим кнопку для первой страницы*/}
+                {typeCountPageNumbers ? <PointPagination focus={currentPage === 1} number={1} paginate={startPage}/> : ''}
+
+                {
+                    // выбираем что рендерить
+                    typeCountPageNumbers ?
+                        // если страниц > 6
+                        dynamicPagination.length === 4 ?
+                            // для рендера начала или конца списка
+                            (dynamicPagination.map((number, index) =>  (
+                                <>
+                                    {(index === 0 && pageNumbers.length > 6 && currentPage > pageNumbers.length - 3) ? <CenterPagination/> : ''}
+                                    <PointPagination focus={currentPage === number} number={number} paginate={paginate}/>
+                                    {(index === dynamicPagination.length - 1 && pageNumbers.length > 6 && currentPage < pageNumbers.length - 2) ? <CenterPagination/> : ''}
+                                </>
+                            )))
+                            :
+                            // для середины списка чтобы отображать текущий и 2 ближних
+                            dynamicPagination.map((number, index) =>  (
+                                <>
+                                    {index === 0 ? <CenterPagination/> : ''}
+                                    <PointPagination focus={currentPage === number} number={number} paginate={paginate}/>
+                                    {index === dynamicPagination.length - 1  ? <CenterPagination/> : ''}
+                                </>
+                            ))
+                    :
+                        // обычная пагинация для < 6
+                        pageNumbers.map(number => (
+                            <PointPagination focus={currentPage === number} number={number} paginate={paginate} />
+                        ))
+                }
+
+                {/*если элементов больше 6, то выводим кнопку для первой страницы*/}
+                {typeCountPageNumbers ? <PointPagination focus={currentPage === pageNumbers[pageNumbers.length - 1]} number={pageNumbers[pageNumbers.length - 1]} paginate={paginate}/> : ''}
             </ul>
-            <li className={style.li} onClick={nextPage}>
+
+            <li className={style.liPrev} onClick={nextPage}>
                 Следующая
             </li>
         </div>
