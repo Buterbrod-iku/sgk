429d6dfb60966207d512731ce19fc6bad119e012
Buterbrod-iku <melkov750@gmail.com>
2023-11-13 21:42:16 +0700

    reset_v2

--- src/app/requests/[id]/page.tsx
+++ src/app/requests/[id]/page.tsx
@@ -16,13 +16,14 @@ import Loading from "@/app/requests/loading/loading";
 import {Change} from "@/app/requests/[id]/change/change";
 import {NoneRequests} from "@/app/requests/page";
 import pathAdaptive from "@/components/static/header/pathAdaptive";
+import {testRequest, testRequestMerged} from "@/app/requests/[id]/test";
 
 
 export const ReversRoutePoint = (request) => {
-    let result = request.orders[0].route.loadingAddress.address
+    let result = request.orders[0].route.loadingAddress.address.split(',')[0]
 
     request.orders.map(item => (
-        result += ' - ' + item.route.unloadingAddress.address
+        result += ' - ' + item.route.unloadingAddress.address.split(',')[0]
     ))
 
     return result
@@ -48,8 +49,20 @@ const findNewPath = async (allRoutes, newPath, setNewPath ,routerId) => {
 export default function OpenRequest (props) {
     const location = usePathname()
     let path = []
-    path = pathAdaptive(location)
-    console.log(path)
+
+    // стэйт для основной заявки, которая будет выводиться
+    let [newRequest, setPost] = useState({})
+    let [server, setServer] = useState(true)
+
+    if(location.split('/')[2] === "qasd4jcyd74hwbnc482"){
+        let hello = testRequest()
+        newRequest = hello
+        server = false
+    } else if(location.split('/')[2] === "vbjn2cvkj532cvjk3df"){
+        let qwe = testRequestMerged()
+        newRequest = qwe
+        server = false
+    }
 
     const router = useParams()
     let routerId = router.id
@@ -60,13 +73,12 @@ export default function OpenRequest (props) {
         if(props.pathId){
             routerId = props.pathId
             fetchPostById(routerId)
-        } else {
+        }
+        else {
             fetchPostById(routerId)
         }
     }, [])
 
-    // стэйт для основной заявки, которая будет выводиться
-    const [newRequest, setPost] = useState({})
 
     // ответ с сервера (приходит массив). Первый объект - сама заявка, остальные - похожие
     const [allRoutes, setAllRoutes] = useState([])
@@ -93,7 +105,18 @@ export default function OpenRequest (props) {
         // если есть похожие заявки, то проходим по ним и достаём id и маршрут
         findNewPath(allRoutes, newPath, setNewPath ,routerId)
 
+        setNewPath(
+            [
+                {
+                    "routeId": "vbjn2cvkj532cvjk3df",
+                    "path": "Тальменка - Новоалтайск"
+                }
+            ]
+        )
+
     }, [allRoutes])
+
+
 
     useEffect(() => {
         const renameTitle = async () => {
@@ -105,10 +128,10 @@ export default function OpenRequest (props) {
     // хз почему, но после запроса может случится такое, что в массиве orders будет null последним элементом
     // в постмене такого нет, но когда получем объект, то он появляется.
     // если null есть, то удаляем его
-    newRequest.orders?.map((item, index) => item === null ? delete newRequest.orders[index] : '')
+    // newRequest.orders?.map((item, index) => item === null ? delete newRequest.orders[index] : '')
 
     const [openInfo, setOpenInfo] = useState(true);
-    const [map, setMap] = useState(true);
+    const [map, setMap] = useState(false);
     const [edit, setEdit] = useState(false);
     const [confirm, setConfirm] = useState(false);
 
@@ -191,27 +214,28 @@ export default function OpenRequest (props) {
                                 <div className={style.info}>
                                     <div className={style.buttonBlock}>
                                         <button onClick={swichInfo} disabled={openInfo} style={openInfo ? {backgroundColor: "rgb(0, 120, 168)", color: "white"} : {backgroundColor: "#ececec", color: "black"}}>Основное</button>
-                                        <button onClick={swichInfo} disabled={!openInfo} style={openInfo ? {backgroundColor: "#ececec", color: "black"} : {backgroundColor: "rgb(0, 120, 168)", color: "white"}}>Груз</button>
+                                        <button onClick={swichInfo} disabled={!openInfo} style={openInfo ? {backgroundColor: "#ececec", color: "black"} : {backgroundColor: "rgb(0, 120, 168)", color: "white"}}>Перевозчик</button>
                                     </div>
 
-                                    <MainInfoRequest setValFunc={setValues} values={values} edit={edit} openInfo={openInfo} allInfo={newRequest}/>
+                                    <MainInfoRequest server={server} setValFunc={setValues} values={values} edit={edit} openInfo={openInfo} allInfo={newRequest}/>
                                 </div>
 
                                 <div className={style.path}>
                                     <div className={style.buttonBlock}>
-                                        <button onClick={swichMap} disabled={map} style={map ? {backgroundColor: "rgb(0, 120, 168)", color: "white"} : {backgroundColor: "#ececec", color: "black"}}>Карта</button>
                                         <button onClick={swichMap} disabled={!map} style={map ? {backgroundColor: "#ececec", color: "black"} : {backgroundColor: "rgb(0, 120, 168)", color: "white"}}>Маршрут</button>
+                                        <button onClick={swichMap} disabled={map} style={map ? {backgroundColor: "rgb(0, 120, 168)", color: "white"} : {backgroundColor: "#ececec", color: "black"}}>Карта</button>
                                     </div>
 
                                     <div className={style.map} style={map ? {display: "block"} : {display: "none"}}>
                                         <div className={style.mapBlock}>
-                                            <iframe className={style.mapFrame} src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d1196030.6502760502!2d82.12100431823762!3d54.16241036132776!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e0!4m5!1s0x42dfe5e190cc4d97%3A0x9b3a0673e1d3e985!2z0J3QvtCy0L7RgdC40LHQuNGA0YHQuiwg0KDQvtGB0YHQuNGP!3m2!1d54.983269299999996!2d82.8963831!4m5!1s0x42dda1e8c72eeeab%3A0xb0e7bbef8d87b503!2z0JHQsNGA0L3QsNGD0LssINCQ0LvRgtCw0LnRgdC60LjQuSDQutGA0LDQuSwg0KDQvtGB0YHQuNGP!3m2!1d53.3497499!2d83.78357369999999!5e0!3m2!1sru!2suk!4v1689234669026!5m2!1sru!2suk" width="600" height="450" loading="lazy"></iframe>
+                                            {/*<iframe className={style.mapFrame} src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d1196030.6502760502!2d82.12100431823762!3d54.16241036132776!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e0!4m5!1s0x42dfe5e190cc4d97%3A0x9b3a0673e1d3e985!2z0J3QvtCy0L7RgdC40LHQuNGA0YHQuiwg0KDQvtGB0YHQuNGP!3m2!1d54.983269299999996!2d82.8963831!4m5!1s0x42dda1e8c72eeeab%3A0xb0e7bbef8d87b503!2z0JHQsNGA0L3QsNGD0LssINCQ0LvRgtCw0LnRgdC60LjQuSDQutGA0LDQuSwg0KDQvtGB0YHQuNGP!3m2!1d53.3497499!2d83.78357369999999!5e0!3m2!1sru!2suk!4v1689234669026!5m2!1sru!2suk" width="600" height="450" loading="lazy"></iframe>*/}
+                                            <Loading />
                                         </div>
                                     </div>
 
                                     <div className={style.route} style={map ? {display: "none"} : {display: "block"}}>
                                         {/*промежуточные точки*/}
-                                        <RoutePoint point={newRequest?.orders[0]?.route?.loadingAddress.address}/>
+                                        <RoutePoint point={newRequest?.orders[0]?.route.loadingAddress.address}/>
                                         {
                                             newRequest?.orders?.map((item, index) => (
                                                 <RoutePoint key={index} point={item.route.unloadingAddress.address}/>
@@ -240,7 +264,7 @@ export default function OpenRequest (props) {
 
 
                             {
-                                newRequest?.route?.isSingle ?
+                                newRequest?.route?.isSingleNEW ?
                                     ""
                                     :
                                     props.newPath ? "" : (
