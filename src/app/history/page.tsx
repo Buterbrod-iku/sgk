09ae3384158df5311e075c63dd813e2a7bf41628
Buterbrod-iku <melkov750@gmail.com>
2023-11-13 19:36:41 +0700

    history

---
+++ src/app/history/page.tsx
@@ -0,0 +1,178 @@
+import style from './history.module.scss';
+import Link from "next/link";
+import Table from "@/app/requests/table/table";
+
+export default function Report() {
+    return (
+        <div className={style.main}>
+            <div className={style.position}>
+                <h3 className={style.title}>История заявок</h3>
+            </div>
+
+            <Table array={request} isLoading={false} history={true}/>
+        </div>
+    )
+}
+
+let request = [
+    {
+        route: {
+            "_id": "qasd4jcyd74hwbnc482",
+            "isSingle": false
+        },
+        orders: [
+            {
+                "date": {
+                    "loadingTime": 1699162938
+                },
+                "order": {
+                    "devisionName": "Подразделение А"
+                },
+                "route": {
+                    "loadingAddress": {
+                        "address": "Кемерово"
+                    },
+                    "unloadingAddress": {
+                        "address": "Бийск"
+                    }
+                }
+            },
+        ]
+    },
+    {
+        route: {
+            "_id": "qasd4jcyd74hwbnc482",
+            "isSingle": false
+        },
+        orders: [
+            {
+                "date": {
+                    "loadingTime": 1699422138
+                },
+                "order": {
+                    "devisionName": "Подразделение А"
+                },
+                "route": {
+                    "loadingAddress": {
+                        "address": "Новосибирск"
+                    },
+                    "unloadingAddress": {
+                        "address": "Барнаул"
+                    }
+                }
+            },
+        ]
+    },
+    {
+        route: {
+            "_id": "qasd4jcyd74hwbnc482",
+            "isSingle": false
+        },
+        orders: [
+            {
+                "date": {
+                    "loadingTime": 1696271100
+                },
+                "order": {
+                    "devisionName": "Подразделение Б"
+                },
+                "route": {
+                    "loadingAddress": {
+                        "address": "Рубцовск"
+                    },
+                    "unloadingAddress": {
+                        "address": "Новоалтайск"
+                    }
+                }
+            },
+        ]
+    },
+    {
+        route: {
+            "_id": "qasd4jcyd74hwbnc482",
+            "isSingle": false
+        },
+        orders: [
+            {
+                "date": {
+                    "loadingTime": 1698554538
+                },
+                "order": {
+                    "devisionName": "Подразделение В"
+                },
+                "route": {
+                    "loadingAddress": {
+                        "address": "Тальменка"
+                    },
+                    "unloadingAddress": {
+                        "address": "Новоалтайск"
+                    }
+                }
+            },
+            {
+                "date": {
+                    "loadingTime": 1698837798
+                },
+                "order": {
+                    "devisionName": "Подразделение А"
+                },
+                "route": {
+                    "loadingAddress": {
+                        "address": "Тальменка"
+                    },
+                    "unloadingAddress": {
+                        "address": "Барнаул"
+                    }
+                }
+            },
+        ]
+    },
+    {
+        route: {
+            "_id": "qasd4jcyd74hwbnc482",
+            "isSingle": false
+        },
+        orders: [
+            {
+                "date": {
+                    "loadingTime": 1698981798
+                },
+                "order": {
+                    "devisionName": "Подразделение А"
+                },
+                "route": {
+                    "loadingAddress": {
+                        "address": "Новосибирск"
+                    },
+                    "unloadingAddress": {
+                        "address": "Новосибирск"
+                    }
+                }
+            },
+        ]
+    },
+    {
+        route: {
+            "_id": "qasd4jcyd74hwbnc482",
+            "isSingle": false
+        },
+        orders: [
+            {
+                "date": {
+                    "loadingTime": 1698837798
+                },
+                "order": {
+                    "devisionName": "Подразделение Г"
+                },
+                "route": {
+                    "loadingAddress": {
+                        "address": "Барнаул"
+                    },
+                    "unloadingAddress": {
+                        "address": "Кемерово"
+                    }
+                }
+            },
+        ]
+    },
+]
--- src/app/requests/lineTable/lineTable.tsx
+++ src/app/requests/lineTable/lineTable.tsx
@@ -1,22 +1,37 @@
 import { useRouter } from 'next/navigation';
 import style from './lineTable.module.scss'
+import download from '../../../assets/images/download_icon_128877.svg'
 
 export default function LineTable(props) {
     const router = useRouter();
 
     const openRequestHandler = e => {
-        router.push(`/requests/${props.requestID}`);
+        if(!props.history){
+            router.push(`/requests/${props.requestID}`);
+        }
     };
 
     return (
         <>
-            <tr onClick={openRequestHandler} className={style.tr}>
+            <tr onClick={openRequestHandler} className={style.tr} style={props.history ? {gridTemplateColumns: "1fr 2fr 4fr 1fr"} : {gridTemplateColumns: "1fr 2fr 5fr"}}>
                 {
                     props.isSingle ? (<div className={style.close}></div>) : ''
                 }
                 <td className={style.th}>{props.date}</td>
                 <td className={style.th}>{props.name}</td>
                 <td className={style.th}>{props.path}</td>
+                {
+                    props.history ? (
+                        <>
+                            <a className={style.a} href={'./'} download={"putevoy_list.xls"}>
+                                <td className={style.th} style={{display: "flex", justifyContent: 'center'}}>
+                                    <img src={download.src}/>
+                                </td>
+                            </a>
+                        </>
+
+                    ) : ''
+                }
             </tr>
         </>
     )
