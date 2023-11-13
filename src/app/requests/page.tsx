2d5a86744369cea3eb7ee840de691b2938eb814e
Buterbrod-iku <melkov750@gmail.com>
2023-11-13 19:36:41 +0700

    history

--- src/app/requests/page.tsx
+++ src/app/requests/page.tsx
@@ -9,6 +9,7 @@ import axios from "axios";
 import {useFetching} from "@/app/hooks/useFetching";
 import PostService from "@/app/API/postService";
 import Loading from "@/app/requests/loading/loading";
+import Table from "@/app/requests/table/table";
 
 const ReversDateTime = (dataTime) => {
     const dateTime = new Date(dataTime * 1000);
@@ -50,88 +51,7 @@ export default function Request() {
         fetchPostGetAll()
     }, [])
 
-    // currentPage - текущая страница пагинации
-    const [currentPage, setCurrentPage] = useState(1)
-    // perPage - сколько объектов будет на одной странице
-    const perPage = 11
 
-    const lastIndex = currentPage * perPage
-    const firstIndex = lastIndex - perPage
-    const [current, setCurrent] = useState([])
-
-    const paginate = (pageNumber) => {
-        setCurrentPage(pageNumber)
-    }
-
-    // функции для кнопок в пагинации
-    const prevPage = () => setCurrentPage(prev => prev <= 1 ? prev : prev - 1)
-    const nextPage = () => setCurrentPage(prev => prev >= appState.length / perPage ? prev : prev + 1)
-    const startPage = () => setCurrentPage(1)
-
-    useEffect(() => {
-        setCurrent(appState.slice(firstIndex, lastIndex))
-    }, [appState, lastIndex])
-
-    const sort = () => {
-        setAppState(appState.sort((a, b) => a.orders[0].date.loadingTime <= b.orders[0].date.loadingTime ? 1 : -1))
-        setCurrent(appState.slice(firstIndex, lastIndex))
-    }
-
-    const altSort = () => {
-        setAppState(appState.sort((a, b) => a.orders[0].date.loadingTime >= b.orders[0].date.loadingTime ? 1 : -1))
-        setCurrent(appState.slice(firstIndex, lastIndex))
-    }
-
-    const name = () => {
-        setAppState(appState.sort((a, b) => a.orders[0].order.devisionName >= b.orders[0].order.devisionName ? 1 : -1))
-        setCurrent(appState.slice(firstIndex, lastIndex))
-    }
-
-    const altName = () => {
-        setAppState(appState.sort((a, b) => a.orders[0].order.devisionName >= b.orders[0].order.devisionName ? -1 : 1))
-        setCurrent(appState.slice(firstIndex, lastIndex))
-    }
-
-    const [date, setDate] = useState(0)
-    const [tc, setTc] = useState(0)
-
-    const dateSort = (e) => {
-        e.preventDefault()
-        if(date === 0){
-            sort()
-            setDate(1)
-            setTc(0)
-        }
-        else if(date === 1){
-            altSort()
-            setDate(2)
-            setTc(0)
-        }
-        else if(date === 2){
-            fetchPostGetAll()
-            setDate(0)
-            setTc(0)
-        }
-    }
-
-    const tcSort = (e) => {
-        e.preventDefault()
-        if(tc === 0){
-            name()
-            setTc(1)
-            setDate(0)
-        }
-        else if(tc === 1){
-            altName()
-            setTc(2)
-            setDate(0)
-        }
-        else if(tc === 2){
-            fetchPostGetAll()
-            setTc(0)
-            setDate(0)
-        }
-    }
 
     return (
         <div className={style.main}>
@@ -143,51 +63,7 @@ export default function Request() {
                 <Link href={"/requests/new"}><button className={style.button}>Создать заявку</button></Link>
             </div>
 
-            {/*<div className={style.sortPosition}>*/}
-            {/*    <SortItem />*/}
-            {/*</div>*/}
-
-            <table className={style.table}>
-                <thead>
-                    <tr className={style.tr}>
-                        <th className={style.date} onClick={dateSort}>
-                            Дата
-                            <div style={date === 0 ? {width: "15px",height: "0"} : date === 2 ? {transform: 'rotate(180deg)'} : {}}></div>
-                        </th>
-                        <th className={style.tc} onClick={tcSort}>
-                            Структурное подразделение
-                            <div style={tc === 0 ? {width: "15px",height: "0"} : tc === 2 ? {transform: 'rotate(180deg)'} : {}}></div>
-                        </th>
-                        <th>Маршрут</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        // проверяем на загрузку
-                        isLoading
-                            ? (<Loading />)
-                            // выводим страницу с заявками
-                            : (
-                                current?.length === 0
-                                    ? (<NoneRequests />)
-                                    :  current?.map(item => {
-                                            return (
-                                                <LineTable key={item.route._id}
-                                                           requestID={item.route._id}
-                                                           date={ReversDateTime(item?.orders[0]?.date.loadingTime)}
-                                                           name={item?.orders[0]?.order?.devisionName}
-                                                           path={ReversRoutePoint(item)}
-                                                           isSingle={item.route.isSingle} />
-                                            )
-                                    })
-                            )
-                    }
-                </tbody>
-            </table>
-            {
-                // компонент пагинации, если страница одна, то ничего не выводим
-                appState.length < perPage ? "" : <Pagination startPage={startPage} currentPage={currentPage} perPage={perPage} totalCount={appState.length} paginate={paginate} nextPage={nextPage} prevPage={prevPage}/>
-            }
+            <Table array={appState} fetchPostGetAll={fetchPostGetAll} isLoading={isLoading}/>
         </div>
     )
 }
